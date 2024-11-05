d11a5bcf466675c8a4b0198e45d57b143678785c
DanielSousaesars <a13672@esars.pt>
2024-11-05 16:58:53 +0000

    Adicionei a função principal da calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -19,3 +19,24 @@ def divisão(x, y):
     if y == 0:
         return ("Erro!")
     return x / y
+
+# Função principal
+def calculadora():
+    operação = input("Escolha a operação: ")
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    if operação == "subtração":
+        result = subtração(num1, num2)
+        print("Resultado da subtração: " + str(result))
+    elif operação == "soma":
+        result = soma(num1, num2)
+        print("Resultado da soma: " + str(result))
+    elif operação == "multiplicação":
+        result = multiplicação(num1, num2)
+        print("Resultado da multiplicação: " + str(result))
+    elif operação == "divisão":
+        result = divisão(num1, num2)
+        print("Resultado da divisão: " + str(result))
+
+# Executar a calculadora
+calculadora()
