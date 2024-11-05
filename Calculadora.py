466933f6fad69cc60eb5935481ab0dc92fe648fb
DanielSousaesars <a13672@esars.pt>
2024-11-05 16:49:58 +0000

    Adicionei as funções das operações para realizar na calculadora

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+
+# Função para soma
+def soma(x, y):
+    return x + y
+
+
+# Função para subtração
+def subtração(x, y):
+    return x - y
+
+
+# Função para multiplicação
+def multiplicação(x, y):
+    return x * y
+
+
+# Função para divisão
+def divisão(x, y):
+    if y == 0:
+        return ("Erro!")
+    return x / y
